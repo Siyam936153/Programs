5316f57e3d807f22953f3f7eb9df144739b8692e
Siyamala S <167208857+Siyam936153@users.noreply.github.com>
2026-03-12 19:45:56 +0530

    Create MiddleLinkedList.java

---
+++ java/MiddleLinkedList.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        int size=0;
+        ListNode temp=head;
+        while(temp!=null){
+            size++;
+            temp=temp.next;
+        }
+        
+        int m=(size/2);
+         temp=head;
+         int count=0;
+         while(count<m){
+            temp=temp.next;
+            count++;
+         }
+         return temp;
+    }
+}
