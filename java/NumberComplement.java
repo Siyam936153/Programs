f91decdfcd2def397e57cbc6a7cebebd9fc9bdf8
Siyamala S <167208857+Siyam936153@users.noreply.github.com>
2026-03-12 18:39:07 +0530

    Implement findComplement method in Solution class

---
+++ java/NumberComplement.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findComplement(int num) {
+        int m=0;
+        int temp=num;
+        while(temp>0){
+            m=(m<<1)+1;
+            temp=temp>>1;
+        }
+        return num^m;
+    }
+}
